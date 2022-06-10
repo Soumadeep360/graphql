265cedf7052ac3a9588ff0314ff02cde2581ea2f
Soumadeep360 <soumadeepsarkar4@gmail.com>
2022-06-10 21:43:26 +0530

    seconf comit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {createNewInfo} from './jqlFunctions/instruction';
+import {createNewInfo,updateInfo,deleteInfo} from './jqlFunctions/instruction';
 
 function App() {
 
-  const hardData={phoneNumber:"8888888888",name:"soumadeep"};
+  const hardData={phoneNumber:"8888888888",name:"Neil"};
+  const hardData1={phoneNumber:"8888888880",name:"soumadeep"};
+  const hardData2={name:"Neil"};
   return (
     <div className="App">
 
       <button onClick={()=>createNewInfo(hardData)}>create new info</button>
+      <button onClick={()=>updateInfo(hardData1)}>update new info</button>
+      <button onClick={()=>deleteInfo(hardData2)}>delete info</button>
     </div>
   );
 }
--- src/jqlFunctions/instruction.js
+++ src/jqlFunctions/instruction.js
@@ -1,8 +1,9 @@
 import * as Mutations from '../graphql/mutations';
-import {API} from 'aws-amplify';
+import {API, Amplify} from 'aws-amplify';
 
-
-
+import awsExports from '../aws-exports';
+// import Amplify from 'aws-amplify';
+Amplify.configure(awsExports);
 export const createNewInfo  = async (infoData) => {
     
     try{
@@ -13,4 +14,29 @@ export const createNewInfo  = async (infoData) => {
      console.log("error info:",error);
     }
 }
+
+    export const updateInfo =async(infoData) => {
+        try{
+            const upInfo=await API.graphql({query:Mutations.updateInstruction,variables:{input:infoData}})
+            console.log("the updated info is: ",upInfo.data.updateInstruction);
+        }
+    
+    catch(error){
+        console.log("error info:",error);
+    }
+}
+
+export const deleteInfo=async(infoData) => {
+    try{
+        console.log("xyz:",infoData);
+        const  delInfo =await API.graphql({query:Mutations.deleteInstruction,variable:{input:infoData.name}})
+        console.log(delInfo);
+        console.log("the deleted info is:",delInfo.data.deleteInstruction); 
+        // console.log("xyz:",infoData);
+    }
+    catch(error){
+        console.log("the error info:",error);
+    }
+}
+
  
